32afe432a90bb0e3814f3e791668f4a7666f834d
rudimarioporto <eletricaportovendas@gmail.com>
2026-04-21 03:22:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
-
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={__BASE_PATH__}>
+      <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
