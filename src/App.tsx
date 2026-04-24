68ff69bda0f9ddb67d194e5874acb7accecb5174
rudimarioporto <eletricaportovendas@gmail.com>
2026-04-24 03:41:04 -0300

    Simplify App component to return a test message
    Removed routing and internationalization setup, returning a test message instead.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./router";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
-
 function App() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </I18nextProvider>
-  );
+  return <h1>TESTE OK</h1>;
 }
 
 export default App;
